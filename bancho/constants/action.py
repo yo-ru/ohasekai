778400fb6d22188c7a21102de4993ef18fdbfd91
Yo-ru <yoru@its.moe>
2022-03-14 20:05:47 -0500

    move and rename actions

---
+++ bancho/constants/action.py
@@ -0,0 +1,32 @@
+from enum import unique
+from enum import IntEnum
+from dataclasses import dataclass
+
+from .mode import Mode
+from .mods import Mods
+
+@unique
+class Action(IntEnum):
+    IDLE = 0
+    AFK = 1
+    PLAYING = 2
+    EDITING = 3
+    MODDING = 4
+    MULTIPLAYER = 5
+    WATCHING = 6
+    UNKNOWN = 7
+    TESTING = 8
+    SUBMITTING = 9
+    PAUSED = 10
+    LOBBY = 11
+    MULTIPLAYING = 12
+    OSUDIRECT = 13
+
+@dataclass
+class ActionData:
+    action: Action = Action.IDLE
+    text: str = ""
+    mapMD5: str = ""
+    mods: Mods = Mods.NOMOD
+    mode: Mode = Mode.OSU
+    mapID: int = 0
